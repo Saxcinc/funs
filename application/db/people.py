69f99c3d2789ffc580dbe3b1d78788ac55c9e59b
sacxinc <irekovichalmaz@gmail.com>
2024-04-29 16:21:03 +0300

    Initial commit

---
+++ application/db/people.py
@@ -0,0 +1,7 @@
+from tqdm import tqdm
+
+
+def get_employees():
+    for progress_bar in tqdm(range(int(90e6))):
+        pass
+
---
+++ application/salary.py
@@ -0,0 +1,6 @@
+import art
+
+
+def calculate_salary():
+    print(art.text2art("Netology", font='block'))
+
